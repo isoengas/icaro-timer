507a1f8ca6d132c9b4669a834b4b238fb742d55c
Soengas Ribote <BISORIIG@intranet.ibermatica>
2017-05-31 15:16:23 +0200

    El localStorage fallaba en iOS con navegación privada

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -2,10 +2,25 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StorageService {
+  private isAvailable = true;
+  constructor() {
+    this.isAvailable = this.isLocalStorageAvailable();
+  }
 
-  constructor() { }
+  private isLocalStorageAvailable(): boolean {
+    try {
+      this.write('test', 1);
+      this.remove('test');
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 
   write(key: string, value: any) {
+    if (!this.isAvailable) {
+      return;
+    }
     if (value) {
       value = JSON.stringify(value);
     }
@@ -13,12 +28,19 @@ export class StorageService {
   }
 
   read<T>(key: string): T {
+    if (!this.isAvailable) {
+      return null;
+    }
     const value: string = localStorage.getItem(key);
 
     if (value && value !== 'undefined' && value !== 'null') {
       return <T>JSON.parse(value);
     }
     return null;
+  }
+
+  remove(key: string) {
+    localStorage.removeItem(key);
   }
 
 }
